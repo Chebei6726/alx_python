6196b07cf68c2db684460b1975941f1009ae6727
Gideon Kipleting <chebei92@gmail.com>
2023-07-19 00:35:57 +0300

    few code edits

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,8 +1,6 @@
 import random
 number = random.randint(-10, 10)
-# Generate a random signed number between -10 and 10 as instructed
-number = random.randint(-10,10)
-# Test if the random number is positive or negative
+number = random.randint(-10, 10)
 if number >= 0:
     print(f"The number {number} is positive.\n")
 else:
