f2447fe4eeb5adf781305b63d162707ebea493e3
Gideon Kipleting <chebei92@gmail.com>
2023-07-19 22:03:47 +0300

    two-number combination in pythons

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def two_digit_combinations():
-    for i in range(10):
-        for j in range(10):
-            print(f"{i}{j}")
+def two_number_combinations():
+    combinations = [f"{i}{j}" for i in range(10) for j in range(10)]
+    print(', '.join(combinations))
 
-two_digit_combinations()
+two_number_combinations()
