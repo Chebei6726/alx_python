478a2a86055cf3a61f0873f979b3489c321c0b58
Gideon Kipleting <chebei92@gmail.com>
2023-07-20 19:56:45 +0300

    a few corrections

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,9 +1,3 @@
+#!/usr/bin/python3
 def pow(a, b):
-    result = 1
-
-    while b > 0:
-        if b % 2 == 1:
-            result *= a
-        a *= a
-        b //= 2
-    return result
+    return a**b
