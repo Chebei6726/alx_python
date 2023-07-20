3fdba76de5b88ab68b7cb26a4acff85fadaaf9f0
Gideon Kipleting <chebei92@gmail.com>
2023-07-20 12:42:46 +0300

    change decimal to hexa

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for number in range(0,99):
-    print("Decimal: {:d}, Hexadecimal: {:02x}".format(number, number))
+    print("Decimal: {:d}, Hexadecimal: {:2x}".format(number, number))
