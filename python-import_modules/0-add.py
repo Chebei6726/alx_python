93b6a4174aa022e06f9833f9953f7aa4d43fa94e
Gideon Kipleting <chebei92@gmail.com>
2023-07-27 13:14:25 +0300

    formatting print statement

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -7,4 +7,4 @@ result= add(a,b)
 output_format="{a} + {b}={result}"
 print(output_format.format(a=a,b=b,result=result))
 if __name__ == '__main__':
-    print("Code from add_0.py executed.")
+    print()
