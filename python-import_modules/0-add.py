7385c99cf9db6440082ac817c167f67a18e47f3f
Gideon Kipleting <chebei92@gmail.com>
2023-07-27 12:53:33 +0300

    condition added

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,5 +3,7 @@ from add_0 import add
 #define a and b
 a=1
 b=2
-result=add(a,b)
+result= add(a,b)
 print(f"{a}+{b}={result}\n")
+if __name__ == '__main__':
+    print("Code from add_0.py executed.")
