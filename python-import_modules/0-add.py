9e07d1ac8951ce673c5b8421c51bb3946dab335e
Gideon Kipleting <chebei92@gmail.com>
2023-07-30 13:05:21 +0300

    final submission

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 from add_0 import add
-#define a and b
-a=1
-b=2
-result= add(a,b)
-output_format="{a} + {b}={result}"
-print(output_format.format(a=a,b=b,result=result))
-if __name__ == '__main__':
-    print()
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    result = add(a, b)
+
+    print("{} + {} = {}".format(a, b, result))
