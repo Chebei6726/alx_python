48513fc2d0636e4f31e26d0b6214d1cb0090f101
Gideon Kipleting <chebei92@gmail.com>
2023-07-31 16:27:36 +0300

    completion of assignments

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
         result = a / b
@@ -11,7 +12,3 @@ def safe_print_division(a, b):
         print("Inside result: {}".format(result))
         return result
 
-# Example usage:
-a = 10
-b = 2
-safe_print_division(a, b)
