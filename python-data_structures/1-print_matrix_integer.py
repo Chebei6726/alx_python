a626066b6d518e81e8143993c48158f007e26217
Gideon Kipleting <chebei92@gmail.com>
2023-08-02 21:58:17 +0300

    matrix formatting

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,8 +3,18 @@ def print_matrix_integer(matrix=[[]]):
         print("Empty matrix")
         return
 
+    max_length = 0
+
+    # Calculate the maximum length of any integer in the matrix
     for row in matrix:
         for num in row:
-            print("{:d} ".format(num), end="")
-        print()
+            num_length = len(str(num))
+            if num_length > max_length:
+                max_length = num_length
 
+    # Print the matrix with proper formatting
+    for row in matrix:
+        for num in row:
+            format_str = "{:>" + str(max_length) + "}"
+            print(format_str.format(num), end=" ")
+        print()  # Move to the next line after each row
